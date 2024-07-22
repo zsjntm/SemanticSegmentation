2ba657c3e7d194caa1a49b6e6219ea844eba6c0a
hecon <h2860142245@gmail.com>
2024-07-22 20:32:22 +0800

    train_cache/built_models/ add

---
+++ train_cache/built_models/cityscapes_models/r18/model.py
@@ -0,0 +1,24 @@
+from models.features import get_r18_feature
+from models.modules import SegModel
+
+
+def build():
+    class Net(SegModel):
+        def __init__(self):
+            super().__init__(512, 19)
+
+            self.feature = get_r18_feature(True, True)
+
+        def forward(self, x):
+            b, c, h, w = x.shape
+            x1, x2, x3, x4 = self.feature(x)
+            return super().forward(x4, (h, w))
+    return Net()
+
+
+if __name__ == '__main__':
+    import torch
+    model = build()
+    x = torch.rand(3, 3, 32, 32)
+    print(model)
+    print(model(x).shape)
